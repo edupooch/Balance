779d48692e05d7fcf624eb8f784873410367ab9d
Eduardo H P Pooch <edupooch@gmail.com>
2017-06-10 18:22:11 -0300

    cria csv e guarda no diretorio padrão

--- app/src/main/java/br/edu/ufcspa/balance/controle/ResultadoActivity.java
+++ app/src/main/java/br/edu/ufcspa/balance/controle/ResultadoActivity.java
@@ -1,10 +1,12 @@
 package br.edu.ufcspa.balance.controle;
 
 import android.content.DialogInterface;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,12 +17,18 @@ import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PanZoom;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import br.edu.ufcspa.balance.R;
 import br.edu.ufcspa.balance.modelo.Avaliacao;
@@ -35,6 +43,8 @@ public class ResultadoActivity extends AppCompatActivity {
     public static final Integer OLHOS_FECHADOS = 2;
     public static final Integer UMA_PERNA = 1;
     public static final Integer DUAS_PERNAS = 2;
+    public static final String DIRETORIO_PADRÃO = "/storage/emulated/0/Balance/";
+    public static final String SEPARADOR = ";";
 
     private TextView textNomePaciente;
     private TextView textIdadePaciente;
@@ -69,7 +79,6 @@ public class ResultadoActivity extends AppCompatActivity {
         textNomePaciente.setText(paciente.getNome());
         textIdadePaciente.setText(Calcula.idadeEmAnos(paciente.getDataNascimento()));
 
-
         textIdadePaciente.setText(Calcula.idadeEmAnos(paciente.getDataNascimento()));
         textArea.setText(String.valueOf(avaliacao.getArea()));
 
@@ -87,11 +96,7 @@ public class ResultadoActivity extends AppCompatActivity {
     }
 
     private void criaGrafico() {
-        Gson gson = new Gson();
-        String jsonArray = avaliacao.getDadosAcelerometro();
-        Type listType = new TypeToken<ArrayList<DadoAcelerometro>>() {
-        }.getType();
-        List<DadoAcelerometro> listaDadosAcelerometro = gson.fromJson(jsonArray, listType);
+        List<DadoAcelerometro> listaDadosAcelerometro = getDadosAcelerometro();
 
         float alturaDoAparelho = 1.20f;
         float maiorX = 0;
@@ -121,6 +126,14 @@ public class ResultadoActivity extends AppCompatActivity {
         PanZoom.attach(plot, PanZoom.Pan.BOTH, PanZoom.Zoom.STRETCH_BOTH, PanZoom.ZoomLimit.OUTER);
     }
 
+    private List<DadoAcelerometro> getDadosAcelerometro() {
+        Gson gson = new Gson();
+        String jsonArray = avaliacao.getDadosAcelerometro();
+        Type listType = new TypeToken<ArrayList<DadoAcelerometro>>() {
+        }.getType();
+        return gson.fromJson(jsonArray, listType);
+    }
+
     private void findViews() {
         textNomePaciente = (TextView) findViewById(R.id.text_nome_paciente);
         textIdadePaciente = (TextView) findViewById(R.id.text_idade_paciente);
@@ -142,8 +155,61 @@ public class ResultadoActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_deletar:
                 criaAlertDialog();
+                break;
+            case R.id.action_exportar:
+                exportaCSV();
+                break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void exportaCSV() {
+        String nomePaciente = textNomePaciente.getText().toString();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-hh-mm", Locale.getDefault());
+        String fileName = nomePaciente.replace(" ", "").toLowerCase() + "-" + sdf.format(avaliacao.getData()) + ".csv";
+
+        File diretorio = criaDiretorio();
+        File file = new File(diretorio, fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("DADOS DA AVALIAÇÃO:\n")
+                .append(nomePaciente).append(SEPARADOR)
+                .append(textIdadePaciente.getText().toString()).append(SEPARADOR)
+                .append(sdf.format(avaliacao.getData())).append(SEPARADOR)
+                .append(textArea.getText().toString()).append(SEPARADOR)
+                .append(textDuracao.getText().toString()).append(SEPARADOR)
+                .append(textOlhos.getText().toString()).append(SEPARADOR)
+                .append(textPernas.getText().toString()).append("\n\n");
+
+        stringBuilder.append("TEMPO;X;Y;Z\n");
+        for (DadoAcelerometro dado : getDadosAcelerometro()) {
+            stringBuilder.append(dado.getTempo()).append(SEPARADOR)
+                    .append(dado.getX()).append(SEPARADOR)
+                    .append(dado.getY()).append(SEPARADOR)
+                    .append(dado.getZ()).append("\n");
+        }
+
+        try {
+            Files.asCharSink(file, Charsets.UTF_8).write(stringBuilder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Log.d("FILE", String.valueOf(Files.readLines(file, Charsets.UTF_8)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private File criaDiretorio() {
+        //DIRETORIO_PADRAO = pasta de mais fácil acesso para o usuário
+        File dir = new File(DIRETORIO_PADRÃO);
+        boolean criou = dir.mkdir();
+        if (!criou) { //RETORNA DIRETORIO PADRÃO DO APP
+            return getFilesDir();
+        }
+        return dir;
     }
 
     private void criaAlertDialog() {
